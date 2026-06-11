6b15557daeb4e6039c2256b1ff554b738286df7a
Apheff <87418010+Apheff@users.noreply.github.com>
2026-06-11 16:45:06 +0200

    update v0.0.2

---
+++ portfolio/src/components/Footer.svelte
@@ -0,0 +1,58 @@
+<script>
+    import Coockie from "$lib/assets/coockie.png"
+    import Link from "./Link.svelte"
+    let { isDark = false } = $props();
+</script>
+
+
+<div class="{isDark ? 'bg-black' : 'bg-white'} {isDark ? 'text-white' : 'text-black'} w-full h-auto p-24">
+    
+    
+    <div class="w-full h-[2px] {isDark ? 'bg-white' : 'bg-black'} mb-16"></div>
+
+    <div class="font-pacaembu font-extralight font text-5xl mb-8">
+        Grazie per essere arrivat* fino a qui, 
+    </div>
+
+    <div class="font-pacaembu text-8xl group cursor-pointer w-full mb-16">
+        <a href="https://tr.ee/0LVNr0QH16" class="items-center gap-6">
+            
+            <!-- Testo con transizione di colore -->
+            <span class="group-hover:text-pinky transition-colors duration-300 w-3/4">
+                SCAMBIAMO DUE
+            </span>
+            
+            <div class="flex items-center">
+                <span class="group-hover:text-pinky transition-colors duration-300 w-auto">
+                    CHIACCHIERE!
+                </span>
+                <!-- Immagine trattata come un'emoji -->
+                <img 
+                    src={Coockie} 
+                    alt="Cookie"
+                    class="w-[0.8em] h-[0.8em] transition-all duration-500 ease-in-out 
+                        opacity-0 group-hover:opacity-100
+                        rotate-0 group-hover:rotate-12 translate-x-[-20px] group-hover:translate-x-0" 
+                />
+            </div>
+        </a>
+    </div>
+
+    <div class="flex h-24 items-center justify-center">
+        <Link
+            color = "white/60"
+            href = "https://www.linkedin.com/in/francesca-catuogno-317b422b3"
+        >
+            Linkedin
+        </Link>
+
+        <div class="bg-white m-4 w-4 h-4 rounded-full"></div>
+        <Link
+            color = "white/60"
+            href = "https://linktr.ee/francesca_catuogno"
+        >
+            Linktr.ee
+        </Link>
+    </div>
+
+</div>
---
+++ portfolio/src/components/Link.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { href = "/", color = "white", children} = $props();
+</script>
+
+<a href={href} class="text-{color} hover:text-pinky transition-colors duration-300 font-arimo font-normal text-5xl">
+    {@render children()}
+</a>
